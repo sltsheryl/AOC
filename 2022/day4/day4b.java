e3efbfa6f56120f90b8d8003a10a387e6aa92ec3
sltsheryl <sltsheryl@gmail.com>
2022-12-06 10:53:53 +0900

    Add day 4

---
+++ 2022/day4/day4b.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class day4b {
+    public static void main(String[] args) {
+        int count = 0;
+        
+        try {
+            File myObj = new File("input.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String firstPart = data.split(",")[0];
+                String secondPart = data.split(",")[1];
+                int firstStart = Integer.parseInt(firstPart.split("-")[0]);
+                int firstEnd = Integer.parseInt(firstPart.split("-")[1]);
+                int secondStart = Integer.parseInt(secondPart.split("-")[0]);
+                int secondEnd = Integer.parseInt(secondPart.split("-")[1]);
+
+                if ((firstStart <= secondStart && firstEnd >= secondStart)
+                        || (firstStart >= secondStart && secondEnd >= firstStart)) {
+                    count++;
+                }
+            }
+            myReader.close();
+        
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        System.out.println(count);
+    }
+}
